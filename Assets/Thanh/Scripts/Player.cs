eabccd4f746215b485dd3a64044bbc520bedc3cb
h3lpmeimdead <vietuc.2004@gmail.com>
2023-10-12 10:45:41 +0700

    b

--- Assets/Thanh/Scripts/Player.cs
+++ Assets/Thanh/Scripts/Player.cs
@@ -3,26 +3,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-    WeaponHolder holder;
+    [SerializeField]
+    private InputActionReference attack, pointer;
+    private WeaponHolder weaponHolder;
+    private Vector2 pointerInput;
+    public Vector2 PointerInput => pointerInput;
     private void Awake()
     {
-        
+        weaponHolder = GetComponentInChildren<WeaponHolder>();
     }
-    // Start is called before the first frame update
-    void Start()
-    {
-        holder = GetComponent<WeaponHolder>();
-    }
-
-    // Update is called once per frame
     private void Update()
     {
-        if(Input.GetMouseButtonDown(0))
-        {
-            holder.Attack();
-        }
+        pointerInput = GetPointerInput();
+        weaponHolder.PointerPosition = pointerInput;
+    }
+    private void OnEnable()
+    {
+        attack.action.performed += PerformAttack;
+    }
+
+    private void PerformAttack(InputAction.CallbackContext context)
+    {
+        weaponHolder.Attack();
+    }
+
+    private void OnDisable()
+    {
+        attack.action.performed -= PerformAttack;
+    }
+    private Vector2 GetPointerInput()
+    {
+        Vector3 mousePos = pointer.action.ReadValue<Vector2>();
+        mousePos.z = Camera.main.nearClipPlane;
+        return Camera.main.ScreenToWorldPoint(mousePos);
     }
 }
--- Assets/Thanh/Scripts/WeaponHolder.cs
+++ Assets/Thanh/Scripts/WeaponHolder.cs
@@ -8,57 +8,42 @@ public class WeaponHolder : MonoBehaviour
     public Animator animator;
     public float delay = 0.3f;
     private bool attackBlocked;
-    public bool IsAttacking { get; private set; }
-    public Transform circle;
-    public float radius;
-    private Camera mainCam;
-    private Vector2 mousePos;
-    public WeaponHolder weaponHolder;
-
-    private void Start()
-    {
-        mainCam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
-    }
+    public Vector2 PointerPosition {  get; set; }
+    public bool isAttacking { get; private set; }
     public void ResetAttack()
     {
-        IsAttacking = false;
+        isAttacking = false;
+    }
 
-    }
-    private void FixedUpdate()
+    public void Update()
     {
-        mousePos = mainCam.ScreenToWorldPoint(Input.mousePosition) - transform.position;
-        float angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
-        Quaternion rotation = Quaternion.Euler(0, 0, angle);
-        transform.rotation = rotation;
-    }
-    private void Update()
-    {
-        if (IsAttacking)
+        if (isAttacking)
         {
             return;
         }
-        Vector2 direction = (mousePos - (Vector2)transform.position).normalized;
+        Vector2 direction = (PointerPosition - (Vector2)transform.position).normalized;
         transform.right = direction;
         Vector2 scale = transform.localScale;
         if(direction.x < 0)
         {
             scale.y = -1;
         }
-        else if( direction.x > 0)
+        else if(direction.x > 0)
         {
             scale.y = 1;
         }
-        transform.localScale = scale;  
-
-        if(transform.eulerAngles.z > 0 && transform.eulerAngles.z < 180)
+        transform.localScale = scale;
+        
+        if(transform.localEulerAngles.z > 180 && transform.localEulerAngles.z <360)
         {
             weaponRenderer.sortingOrder = characterRenderer.sortingOrder - 1;
-        } 
+        }
         else
         {
             weaponRenderer.sortingOrder = characterRenderer.sortingOrder + 1;
         }
     }
+
     public void Attack()
     {
         if(attackBlocked)
@@ -66,26 +51,13 @@ public class WeaponHolder : MonoBehaviour
             return;
         }
         animator.SetTrigger("Attack");
-        IsAttacking = true;
         attackBlocked = true;
+        isAttacking = true;
         StartCoroutine(DelayAttack());
     }
     private IEnumerator DelayAttack()
     {
         yield return new WaitForSeconds(delay);
         attackBlocked = false;
-    }
-    private void OnDrawGizmosSelected()
-    {
-        Gizmos.color = Color.blue;
-        Vector3 position = circle == null ? Vector3.zero : circle.position;
-        Gizmos.DrawWireSphere(position, radius);
-    }
-    public void DetectCol()
-    {
-        foreach(Collider2D collider in Physics2D.OverlapCircleAll(circle.position,radius))
-        {
-            Debug.Log(collider.name);
-        }
     }
 }
