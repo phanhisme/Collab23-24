50be5ec8c173ba485dbf1c97e8310b6cc7df74af
phanhisme <phanhhisme@gmail.com>
2023-10-24 04:00:00 +0700

    24.10 Collect Update

---
+++ Assets/Phanh's Work/PA_Script/CoinCollection/MagnetCoin.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagnetCoin : MonoBehaviour
+{
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        //check if the game object is a coin
+        if(collision.gameObject.TryGetComponent<Coin>(out Coin coin))
+        {
+            //find player's location
+            coin.FindTarget(transform.parent.position);
+        }
+    }
+}
