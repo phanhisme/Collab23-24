8038cf2e24ac7ec920bc247d8c58b2f8359cb9e8
dat51277 <truykich.captain@gmail.com>
2023-11-09 20:08:25 +0700

    fix mn

--- Assets/DatW/Scripts/MenuUI.cs
+++ Assets/DatW/Scripts/MenuUI.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Button : MonoBehaviour
+public class MenuUI : MonoBehaviour
 {
     public GameObject optionMenu;
     public GameObject mainMenu;
