55adeb175ac934cacaf77b25aa74326dfbbf6348
dat51277 <truykich.captain@gmail.com>
2023-11-19 14:47:48 +0700

    save test

--- Assets/DatW/Scripts/Serialization/SerializationManager.cs
+++ Assets/DatW/Scripts/Serialization/SerializationManager.cs
@@ -1,18 +1,64 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 using UnityEngine;
 
-public class SerializationManager : MonoBehaviour
+public class SerializationManager
 {
-    // Start is called before the first frame update
-    void Start()
+    public static bool Save(string saveName, object saveData)
     {
-        
+        BinaryFormatter formatter = GetBinaryFormatter();
+
+        if (!Directory.Exists(Application.persistentDataPath + "/saves"))
+        {
+            Directory.CreateDirectory(Application.persistentDataPath + "/saves");
+        }
+
+        string path = Application.persistentDataPath + "/saves" + saveName + ".saves";
+
+        FileStream file = File.Create(path);
+
+        formatter.Serialize(file, saveData);
+
+        file.Close();
+
+        return true;
     }
 
-    // Update is called once per frame
-    void Update()
+
+    public static object Load(string path)
     {
-        
+        if (!File.Exists(path))
+        {
+            return null;
+        }
+
+        BinaryFormatter formatter = GetBinaryFormatter();
+
+        FileStream file = File.Open(path, FileMode.Open);
+
+        try
+        {
+            object save = formatter.Deserialize(file);
+            file.Close();
+            return save;
+        }
+        catch
+        {
+            Debug.LogErrorFormat("F to load file at {0}", path);
+            file.Close();
+            return null;
+        }
     }
+
+
+
+    public static BinaryFormatter GetBinaryFormatter()
+    {
+        BinaryFormatter formatter = new BinaryFormatter();
+
+        return formatter;
+    }
+
 }
