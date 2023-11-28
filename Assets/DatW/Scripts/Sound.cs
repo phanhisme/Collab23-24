b2d9235e2690af5f209a7d29a51bfeec116bd757
dat51277 <105587607+dat51277@users.noreply.github.com>
2023-11-28 14:47:46 +0700

    Fig

--- Assets/DatW/Scripts/Sound.cs
+++ Assets/DatW/Scripts/Sound.cs
@@ -5,6 +5,6 @@ using UnityEngine;
 [System.Serializable]
 public class Sound : MonoBehaviour
 {
-    public string name;
-    public AudioClip clip;
+    //public string name;
+    //public AudioClip clip;
 }
