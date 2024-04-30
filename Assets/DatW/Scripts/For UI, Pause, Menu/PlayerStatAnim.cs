5d30fb59fafc9d8a6e605c566d48cb24430833de
dat51277 <truykich.captain@gmail.com>
2024-04-30 22:56:01 +0700

    dang do 2

---
+++ Assets/DatW/Scripts/For UI, Pause, Menu/PlayerStatAnim.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerStatAnim : MonoBehaviour
+{
+    //public Animator animator; // Reference to the Animator component of the UI element
+    //public Button button; // Reference to the Button component
+
+    //void Start()
+    //{
+    //    // Get the Animator component attached to the UI element
+    //    animator = GetComponent<Animator>();
+
+    //    // Get the Button component attached to the button GameObject
+    //    button = GetComponent<Button>();
+
+    //    // Add listener to the button's onClick event
+    //    button.onClick.AddListener(PlayAnimation);
+    //}
+
+    //void PlayAnimation()
+    //{
+    //    // Trigger the "PlayAnimation" parameter in the animator
+    //    animator.SetTrigger("PlayAnimation");
+    //}
+}
