e587ced7538cfb90765e0180e1ca62c2d882833c
dat51277 <truykich.captain@gmail.com>
2024-04-30 16:34:15 +0700

    dang do

--- Assets/DatW/Scripts/For UI, Pause, Menu/QuestManager.cs
+++ Assets/DatW/Scripts/For UI, Pause, Menu/QuestManager.cs
@@ -1,18 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class QuestManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    //public GameObject questPrefab;
+    //public Transform contentPanel;
 
-    // Update is called once per frame
-    void Update()
+    //// Public method to add a quest
+    //public void AddQuest(string questName)
+    //{
+    //    GameObject questItem = Instantiate(questPrefab);
+    //    questItem.transform.SetParent(contentPanel);
+    //    questItem.GetComponentInChildren<Text>().text = questName;
+    //}
+    [System.Serializable]
+    public class Quest
     {
-        
+        public string questName;
+        public string questDescription;
+        // Add other quest properties here
     }
+    
+        public List<Quest> quests = new List<Quest>();
+
+        public void AddQuest()
+        {
+            Quest newQuest = new Quest();
+            newQuest.questName = "New Quest";
+            newQuest.questDescription = "New Description";
+            quests.Add(newQuest);
+        }
+    
 }
