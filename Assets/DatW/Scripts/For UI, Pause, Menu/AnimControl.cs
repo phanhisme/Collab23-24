c92bbfeb6ffbbcc547ffda76b0bbd77a34eaf27b
dat51277 <105587607+dat51277@users.noreply.github.com>
2024-05-02 14:43:58 +0700

    3.2

--- Assets/DatW/Scripts/For UI, Pause, Menu/AnimControl.cs
+++ Assets/DatW/Scripts/For UI, Pause, Menu/AnimControl.cs
@@ -5,19 +5,27 @@ using UnityEngine;
 public class AnimControl : MonoBehaviour
 {
     private Animator animator;
-
     void Start()
     {
         // Get the Animator component attached to the GameObject
         animator = GetComponent<Animator>();
 
-        // Stop the animation at the start
-        animator.enabled = false;
+        // Stop all animations at the start
+        animator.SetBool("WeapChooseAnim", false);
+        animator.SetBool("WeapChooseCloseAnim", false);
     }
 
-    //public void PlayAnimation()
-    //{
-    //    // Play the animation
-    //    animator.enabled = true;
-    //}
+    public void PlayAnimation1()
+    {
+        // Stop other animations and play Animation1
+        animator.SetBool("WeapChooseAnim", true);
+        animator.SetBool("WeapChooseCloseAnim", false);
+    }
+
+    public void PlayAnimation2()
+    {
+        // Stop other animations and play Animation2
+        animator.SetBool("WeapChooseAnim", false);
+        animator.SetBool("WeapChooseCloseAnim", true);
+    }
 }
