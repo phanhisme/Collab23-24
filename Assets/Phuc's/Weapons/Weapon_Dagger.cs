3edf9899178ea5b275a58d91d55e9d5c290b9032
Phucego <146084694+Phucego@users.noreply.github.com>
2024-05-30 11:49:30 +0700

    30/5 sprites

--- Assets/Phuc's/Weapons/Weapon_Dagger.cs
+++ Assets/Phuc's/Weapons/Weapon_Dagger.cs
@@ -2,10 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WeaponSword : WeaponBase
+public class Weapon_Dagger : WeaponBase
 {
-
-    public override void Attack(Animator anim)
+    public override void Attack(Animator animator)
     {
         
     }
