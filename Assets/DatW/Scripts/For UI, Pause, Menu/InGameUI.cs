cc2b8baaee0beac1f03a1f177aeda9d1a71bd25f
dat51277 <105587607+dat51277@users.noreply.github.com>
2024-06-06 14:34:54 +0700

    Ingameui

--- Assets/DatW/Scripts/For UI, Pause, Menu/InGameUI.cs
+++ Assets/DatW/Scripts/For UI, Pause, Menu/InGameUI.cs
@@ -5,6 +5,13 @@ using UnityEngine;
 public class InGameUI : MonoBehaviour
 {
     public GameObject QuestMenu;
+    // Reference to the Animator components
+    public Animator animator;
+  
+
+    // The names of the animations to be triggered
+    public string animationName1;
+    public string animationName2;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,7 +21,8 @@ public class InGameUI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //someUIButtonReference.onClick.AddListener(SomeFunction1);
+        //someUIButtonReference.onClick.AddListener(SomeFunction2);
     }
     public void Quest()
     {
@@ -28,5 +36,13 @@ public class InGameUI : MonoBehaviour
     public void LoadOutIG()
     {
 
+    }
+    public void PlayAnimations()
+    {
+        if (animator != null)
+        {
+            animator.Play(animationName1);
+            animator.Play(animationName2);
+        }
     }
 }
